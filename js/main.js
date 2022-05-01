5cab386854959ad98e5e4448b47dc886786e5b1b
espnal <78657757+espnal@users.noreply.github.com>
2022-05-01 00:52:46 -0400

    latest

--- js/main.js
+++ js/main.js
@@ -9,8 +9,40 @@ const links = [{
         url: "week2/index.html"
     },
     {
-        label: "Week3 work",
-        url: "week3/index.html"
+        label: "Week4 work",
+        url: "week4/index.html"
+    },
+    {
+        label: "Week5 work",
+        url: "week5/index.html"
+    },
+    {
+        label: "Week6 work",
+        url: "week6/index.html"
+    },
+    {
+        label: "Week7 work",
+        url: "week7/index.html"
+    },
+    {
+        label: "Week8 work",
+        url: "week8/index.html"
+    },
+    {
+        label: "Week9 work",
+        url: "week9/index.html"
+    },
+    {
+        label: "Week10 work",
+        url: "week10/index.html"
+    },
+    {
+        label: "Week11 work",
+        url: "week11/index.html"
+    },
+    {
+        label: "Week12 work",
+        url: "week12/index.html"
     }
 ];
 
