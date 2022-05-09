432954309edf98ceb0001aaac5c5637e22d9c403
espnal <78657757+espnal@users.noreply.github.com>
2022-05-08 23:41:34 -0400

    latest

--- js/main.js
+++ js/main.js
@@ -9,39 +9,39 @@ const links = [{
         url: "week2/index.html"
     },
     {
-        label: "Week4 work",
+        label: "Week3 work",
         url: "week4/index.html"
     },
     {
-        label: "Week5 work",
+        label: "Week4 work",
         url: "week5/index.html"
     },
     {
-        label: "Week6 work",
+        label: "Week5 work",
         url: "week6/index.html"
     },
     {
-        label: "Week7 work",
+        label: "Week6 work",
         url: "week7/index.html"
     },
     {
-        label: "Week8 work",
+        label: "Week7 work",
         url: "week8/index.html"
     },
     {
-        label: "Week9 work",
+        label: "Week8 work",
         url: "week9/index.html"
     },
     {
-        label: "Week10 work",
+        label: "Week9 work",
         url: "week10/index.html"
     },
     {
-        label: "Week11 work",
+        label: "Week10 work",
         url: "week11/index.html"
     },
     {
-        label: "Week12 work",
+        label: "Week11 work",
         url: "week12/index.html"
     }
 ];
