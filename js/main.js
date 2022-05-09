dbc3679d55f2a88ec38c55bfdb3a0bdd7a4e3df3
espnal <78657757+espnal@users.noreply.github.com>
2022-05-08 23:42:56 -0400

    .

--- js/main.js
+++ js/main.js
@@ -10,39 +10,39 @@ const links = [{
     },
     {
         label: "Week3 work",
-        url: "week4/index.html"
+        url: "week3/index.html"
     },
     {
         label: "Week4 work",
-        url: "week5/index.html"
+        url: "week4/index.html"
     },
     {
         label: "Week5 work",
-        url: "week6/index.html"
+        url: "week5/index.html"
     },
     {
         label: "Week6 work",
-        url: "week7/index.html"
+        url: "week6/index.html"
     },
     {
         label: "Week7 work",
-        url: "week8/index.html"
+        url: "week7/index.html"
     },
     {
         label: "Week8 work",
-        url: "week9/index.html"
+        url: "week8/index.html"
     },
     {
         label: "Week9 work",
-        url: "week10/index.html"
+        url: "week9/index.html"
     },
     {
         label: "Week10 work",
-        url: "week11/index.html"
+        url: "week10/index.html"
     },
     {
         label: "Week11 work",
-        url: "week12/index.html"
+        url: "week11/index.html"
     }
 ];
 
